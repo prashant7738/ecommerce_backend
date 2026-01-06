6d861b920722c30dba5a0ee33e2eee7c37b1c1f2
Prashant7738 <prashantkafle7738@gmail.com>
2026-01-06 11:08:24 +0545

    added cloudinary based image url system and added . env file to make this code more secure

--- store/models.py
+++ store/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     description = models.TextField()
+    image = models.ImageField(upload_to='products/',blank= True , null = True)
     
     def __str__(self):
         return self.product_name
--- store/serializers.py
+++ store/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = ['product_name','price','description','image']
         
         
         
